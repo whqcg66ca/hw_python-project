fa9fbaf06e2ada20384581cb00ed2b41b2146a2b
whqcg66ca <wanghongquan66@gmail.com>
2025-02-06 20:52:19 -0700

    RF Feb

--- PRR_Feb2024/Root_RF_v8_profile_ARR.py
+++ PRR_Feb2024/Root_RF_v8_profile_ARR.py
@@ -1,38 +1,30 @@
-import numpy as np
 import sys
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.cross_decomposition import PLSRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor 
+from sklearn.metrics import accuracy_score, make_scorer
 from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVR  # Import SVM Regressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor  
+from sklearn.preprocessing import StandardScaler as xscaler # includes the preprocessing,standardscaler is to prepare the training dataset
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, root_mean_squared_error
 
-import pickle
+from xgboost import XGBRegressor
 
 import tensorflow as tf
-from keras.layers import Dense
 from keras import Sequential
+from keras.layers import Dense
+import pickle
 
-
-
-# from sklearn.metrics import r2_score, mean_squared_error
-
-# Function to evaluate model performance (similar to nan_stat_eva_model)
-# def nan_stat_eva_model(y_pred, y_true):
-#     R = np.corrcoef(y_pred.flatten(), y_true)
-#     bias = np.mean(y_pred - y_true)
-#     sd = np.std(y_pred - y_true)
-#     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-#     mae = np.mean(np.abs(y_pred - y_true))
-#     d = np.sum((y_pred - y_true) ** 2)
-#     R2 = r2_score(y_true, y_pred)
-#     return R, bias, sd, rmse, mae, d, R2
-
-sys.path.append(r'L:\HSI_Root_Rot\Method\funs')
-from calculate_metrics import nan_stat_eva_model  
-
-# Read the hyperspectral data
+ 
+# %% Step 1: Read the Hyperspectral Shoot data in Excel
 file_path = 'L:/HSI_Root_Rot/Data/HSI Spectra RootRot_MAIN.xlsx'
 arr_2024_shoot = pd.read_excel(file_path, sheet_name='ARR_2024_Shoot').values
 
@@ -51,26 +43,6 @@ plt.ylabel('Reflectance')
 plt.legend()
 plt.show()
 
-# # Plot Reflectance for Rep1
-# plt.figure()
-# plt.plot(waveleth, arr_shoot_rep1[:, 0], '-k', label='Rep1 1')
-# plt.plot(waveleth, arr_shoot_rep1[:, 1], '-r', label='Rep1 2')
-# plt.plot(waveleth, arr_shoot_rep1[:, 2], '-b', label='Rep1 3')
-# plt.xlabel('Wavelength (nm)')
-# plt.ylabel('Reflectance')
-# plt.legend()
-# plt.show()
-
-# # Plot Reflectance for Rep2
-# plt.figure()
-# plt.plot(waveleth, arr_shoot_rep2[:, 0], '-k', label='Rep2 1')
-# plt.plot(waveleth, arr_shoot_rep2[:, 1], '-r', label='Rep2 2')
-# plt.plot(waveleth, arr_shoot_rep2[:, 2], '-b', label='Rep2 3')
-# plt.xlabel('Wavelength (nm)')
-# plt.ylabel('Reflectance')
-# plt.legend()
-# plt.show()
-
 # Read root data
 arr_2024_root = pd.read_excel(file_path, sheet_name='ARR_2024_Root').values
 arr_root_cont = arr_2024_root[:, 1:17]
@@ -87,26 +59,6 @@ plt.ylabel('Reflectance')
 plt.legend()
 plt.show()
 
-# # Plot Root Rep1
-# plt.figure()
-# plt.plot(waveleth, arr_root_rep1[:, 0], '-k', label='Rep1 1')
-# plt.plot(waveleth, arr_root_rep1[:, 1], '-r', label='Rep1 2')
-# plt.plot(waveleth, arr_root_rep1[:, 2], '-b', label='Rep1 3')
-# plt.xlabel('Wavelength (nm)')
-# plt.ylabel('Reflectance')
-# plt.legend()
-# plt.show()
-
-# # Plot Root Rep2
-# plt.figure()
-# plt.plot(waveleth, arr_root_rep2[:, 0], '-k', label='Rep2 1')
-# plt.plot(waveleth, arr_root_rep2[:, 1], '-r', label='Rep2 2')
-# plt.plot(waveleth, arr_root_rep2[:, 2], '-b', label='Rep2 3')
-# plt.xlabel('Wavelength (nm)')
-# plt.ylabel('Reflectance')
-# plt.legend()
-# plt.show()
-
 # Root/Shoot Reflectance ratio
 rr = arr_2024_root[:, 1:]
 ss = arr_2024_shoot[:, 1:]
@@ -122,33 +74,48 @@ xx_shoot = np.concatenate([arr_shoot_cont.T, arr_shoot_rep1.T, arr_shoot_rep2.T]
 xx_root = np.concatenate([arr_root_cont.T, arr_root_rep1.T, arr_root_rep2.T])
 yy = arr_truth[:, 6]
 
-# Model training and validation
-X = xx_shoot
-X_col = X[:, 1]
-ind1 = np.isnan(X_col)
+#%% Step 2:Preprocessing 
+###############################################
+# Option 2: Remove invaludate values
+X = xx_root
+X = X[:, :-3]
 y = yy.astype(float)
-ind2 = np.isnan(y)
-ind = np.unique(np.concatenate([np.where(ind1)[0], np.where(ind2)[0]]))
 
-X = np.delete(X, ind, axis=0)
-y = np.delete(y, ind)
+# Remove NaN values
+nan_mask = ~np.isnan(X[:, 1]) & ~np.isnan(y)
+X = X[nan_mask]
+y = y[nan_mask]
+###############################################
 
-# Splitting the data
-split_ratio = 0.8
-split_idx = np.random.permutation(len(X))
-train_idx = split_idx[:int(split_ratio * len(X))]
-test_idx = split_idx[int(split_ratio * len(X)):]
+##################################################
+# Option -2:  Split the training and test dateset
+# Set random seed for reproducibility
+np.random.seed(50)
+# Split data into training and testing sets
+splitRatio = 0.8
+splitIdx = np.random.permutation(len(X))
+trainIdx = splitIdx[:int(splitRatio * len(X))]
+testIdx = splitIdx[int(splitRatio * len(X)):] 
+X_train, X_test = X[trainIdx], X[testIdx]
+y_train, y_test = y[trainIdx], y[testIdx]
+###############################################
 
-X_train, X_test = X[train_idx], X[test_idx]
-y_train, y_test = y[train_idx], y[test_idx]
 
-#%% Test different AI ML algorithms
+#%% Step 3: Test different AI ML algorithms
+
+
+# Feature Scaling for x, rather than y
+sc = xscaler() # replace the standardscaler as sc
+x_train = sc.fit_transform(X_train) # maybe better to change to different varibale name, standardscaler.fit_transform is scale the training dataset
+x_test = sc.transform(X_test) # standardscaler.transform is to scale the test dataset. It is reasonable that both the training and test datasets need to scaled in the same method
+
 
 # Define the parameter grid for GridSearchCV
 param_grid = {
     'n_estimators': range(5,100,20),      # Number of trees
     'max_depth': [10, 20, 30],     # Maximum depth of each tree
-    'max_leaf_nodes': [10, 20, 30] # Maximum number of leaf nodes
+    'max_leaf_nodes': [10, 20, 30], # Maximum number of leaf nodes
+    'max_features': ['auto', 'sqrt', 'log2', None]
 }
 
 # Initialize the Random Forest Regressor model
@@ -159,28 +126,33 @@ grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid,
                            cv=5, scoring='neg_mean_squared_error', n_jobs=-1, verbose=2)
 
 # Fit the GridSearchCV
-grid_search.fit(X_train, y_train)
+grid_search.fit(x_train, y_train)
 
 # Get the best parameters and model from grid search
 best_params = grid_search.best_params_
 best_rf_model = grid_search.best_estimator_
 
 # Predict using the best model
-y_pred = best_rf_model.predict(X_test)
+y_pred = best_rf_model.predict(x_test)
 
 
 #%% Evaluate the performance of the algorithms
+#%% Step 4: Evaluate the performance of the algorithms
 
 # Evaluate model performance
-R, bias, sd, rmse_s, mae, d, R2 = nan_stat_eva_model(y_pred.flatten(), y_test)
-print(f'R on Test Data: {R[0, 1]:.4f}')
-print(f'RMSE: {rmse_s:.4f}, MAE: {mae:.4f}, R2: {R2:.4f}')
+r_squared = r2_score(y_test, y_pred.flatten())
+rmse = root_mean_squared_error(y_test, y_pred.flatten())
+cor = np.corrcoef(y_test, y_pred.flatten())
+
+print(f'R2 on Test Data: {r_squared:.4f}')
+print(f'RMSE: {rmse:.4f}')
+
 
 # Plot actual vs predicted
 plt.figure()
 plt.scatter(y_test, y_pred, c='k', marker='o')
-plt.text(6, 1.5, f'R = {R[0,1]:.2f}')
-plt.text(6, 1, f'RMSE = {rmse_s:.2f}')
+plt.text(6, 1.5, rf'$R^2 = {r_squared:.2f}$')
+plt.text(6, 1, f'RMSE = {rmse:.2f}')
 plt.xlabel('Visual Rating')
 plt.ylabel('Estimated Root Rot')
 plt.title('Pea Root Rot')
@@ -189,31 +161,5 @@ plt.ylim([0, 8])
 plt.show()
 
 # Save the model to a file
-with open(r'L:\HSI_Root_Rot\Method\rf_model2.pkl', 'wb') as f:
-    pickle.dump(rf_model, f)
-    
-
-#%% Load and use the models
-
-# Later, load the model back from the file
-with open(r'L:\HSI_Root_Rot\Method\ann_model2.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
-    
-
-y_pred = loaded_model.predict(X_test)
-
-# Evaluate model performance
-R, bias, sd, rmse_s, mae, d, R2 = nan_stat_eva_model(y_pred, y_test)
-print(f'R on Test Data: {R[0, 1]:.4f}')
-
-# Plot actual vs predicted
-plt.figure()
-plt.scatter(y_test, y_pred, c='k', marker='o')
-plt.text(6, 1.5, f'R = {R[0,1]:.2f}')
-plt.text(6, 1, f'RMSE = {rmse_s:.2f}')
-plt.xlabel('Visual Rating')
-plt.ylabel('Estimated Root Rot')
-plt.title('Pea Root Rot')
-plt.xlim([0, 8])
-plt.ylim([0, 8])
-plt.show()
+# with open(r'L:\HSI_Root_Rot\Method\rf_model2.pkl', 'wb') as f:
+#     pickle.dump(rf_model, f)
