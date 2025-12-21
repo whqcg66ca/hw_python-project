5df984a38d59b5212fcf16e3a50edf0731fd6987
whqcg66ca <wanghongquan66@gmail.com>
2025-12-20 21:54:52 -0700

    upush

--- SAR _Agriculture/Step2_convert_snap_img_to_tif.py
+++ SAR _Agriculture/Step2_convert_snap_img_to_tif.py
@@ -66,7 +66,7 @@ def convert_img_folder_to_tif(in_folder: str, out_folder: str, compress: str = "
 
 
 if __name__ == "__main__":
-    dd=r"20230620"
+    dd=r"20230714"
     in_folder = "D:\\7_Sentinel\\North2023_Re\\" + dd + "\\S1A_" + dd + "_TC.data"
     zonal_folder = os.path.join(r"D:\7_Sentinel\North2023_Re\\" + dd + "\\", "Zonal")
     if not os.path.isdir(zonal_folder):
--- SAR _Agriculture/Step3_Zonal_statistic7_used.py
+++ SAR _Agriculture/Step3_Zonal_statistic7_used.py
@@ -7,7 +7,7 @@ arcpy.env.overwriteOutput = True
 arcpy.env.addOutputsToMap = False  # helps avoid Pro UI overhead
 
 # --- MODIFY THESE PATHS ---
-in_folder   = r"D:\7_Sentinel\North2023_Re\20230620\Zonal"
+in_folder   = r"D:\7_Sentinel\North2023_Re\20230714\Zonal"
 out_folder  = in_folder
 in_polygons = r"D:\2_2019-2023_NewLiskeard_data_organized\Shapfile_merged2023\All_Polygon.shp"
 zone_field  = "Field"
